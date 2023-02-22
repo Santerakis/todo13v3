808a02bf86cc17ebd22f0f3f52395bd8f6b5b9f2
Sergey stava <santerakis@gmail.com>
2023-02-22 19:22:28 +0200

    model

--- src/api/task-api.ts
+++ src/api/task-api.ts
@@ -1,22 +1,27 @@
 import axios from "axios";
 
-const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+
+const settings = {
     withCredentials: true,
     headers: {
         // Не забываем заменить API-KEY на собственный
         'API-KEY': '794181ab-6d62-4cfb-bc9f-d539dfac55f1',
     },
+}
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    ...settings  //выбираем элементы
 })
 
 export const taskApi = {
     getTasks(todoId: string) {
-        return instance.get<TaskGetResponseType<TaskType>>(`/todo-lists/${todoId}/tasks`)
+        return instance.get<TasksGetResponseType<TaskType>>(`/todo-lists/${todoId}/tasks`)
     },
     createTask(todoId: string, title: string) {
         return instance.post<TaskResponseType<{item: TaskType}>>(`/todo-lists/${todoId}/tasks`, {title})
     },
-    updateTask(todoId: string, taskId: string, title: string) {
+    updateTask(todoId: string, taskId: string, model: UpdateTaskModelType) {
         return instance.put<TaskResponseType<{item: TaskType}>>(`/todo-lists/${todoId}/tasks/${taskId}`, {title})
     },
     deleteTask(todoId: string, taskId: string) {
@@ -30,7 +35,7 @@ export type TaskType = {
     completed: boolean
     status: number
     priority: number
-    startDate: Date
+    startDate: Date  //димыч писал string
     deadline: Date
     id: string
     todoListId: string
@@ -38,13 +43,23 @@ export type TaskType = {
     addedDate: Date
 }
 
-export type TaskGetResponseType<T = {}> = {
-    error: string
-    items: T[]
+export type TasksGetResponseType<T = {}> = {
+    error: string | null  //нас определили по ответуб ане докуметация
+    items: T[]   //димыч items :TaskType[]
     totalCount: number
 }
 
-type TaskResponseType<T = {}>  = {
+type TaskResponseType<T = {}>  = {  //можно проэкспортировать из todo
     data: T
     resultCode: number
+    messages: string[]
+}
+
+type UpdateTaskModelType = {
+    title: string
+    description: string
+    status: number
+    priority: number
+    startDate: string
+    deadline: string
 }
--- src/stories/tasks-api.stories.tsx
+++ src/stories/tasks-api.stories.tsx
@@ -14,7 +14,7 @@ export const GetTasks = () => {
         const todoId = '5e9fda75-294a-401d-a5b3-d85b78b04477'
         taskApi.getTasks(todoId)
             .then((res) => {
-                setState(res)
+                setState(res.data)
                 // setTitle(res.data.items[0].title)
             })
     }, [])
@@ -50,14 +50,21 @@ export const UpdateTask = () => {
 }
 export const DeleteTask = () => {
     const [state, setState] = useState<any>(null)
-    useEffect(() => {
-        const todoId = '5e9fda75-294a-401d-a5b3-d85b78b04477'
-        const taskId = 'aa7a68f4-5b16-4ac2-ad71-cb66bc9d6867'
-        taskApi.deleteTask(todoId, taskId)
+    const [taskId, setTaskId] = useState<string>("")
+    const [todolistId, setTodolistId] = useState<string>("")
+
+    const deleteTask = () => {
+        taskApi.deleteTask(todolistId, taskId)
             .then((res) => {
                 setState(res.data)
             })
-    }, [])
+    }
 
-    return <div>{JSON.stringify(state)}</div>
+    return <div>{JSON.stringify(state)}
+    <div>
+        <input placeholder={'todolistId'} value={todolistId} onChange={(e) => setTodolistId(e.currentTarget.value)}/>
+        <input placeholder={'taskId'} value={taskId} onChange={(e) => setTaskId(e.currentTarget.value)}/>
+        <button onClick={deleteTask}>delete task</button>
+    </div>
+    </div>
 }
